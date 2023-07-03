ec7c5087c6c1f177f2d60fda88e1d8cf53356c1f
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2023-07-03 18:44:40 +0300

    Teleport commit

---
+++ Assets/Script/Teleport.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] Transform destination;
+    GameObject player;
+    private void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")){
+            player.transform.position = destination.transform.position;
+        }
+    }
+}
