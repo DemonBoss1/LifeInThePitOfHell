62eb2c72daa1554dc6c4d53a6ce3748c8d2da539
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2023-07-03 20:27:29 +0300

    UI HP update commit

--- Assets/Script/UIHealthBar.cs
+++ Assets/Script/UIHealthBar.cs
@@ -6,17 +6,12 @@ using UnityEngine.UI;
 public class UIHealthBar : MonoBehaviour
 {
     public static UIHealthBar instance {get; private set;}
-    public Image mask;
-    float originalSize;
+    public Image image;
+
     private void Awake() {
         instance = this;
     }
-    void Start()
-    {
-        originalSize = mask.rectTransform.rect.width;
-    }
-
     public void SetValue(float value){
-        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+        image.fillAmount = value;
     }
 }
