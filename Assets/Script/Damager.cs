31bee181a330819885d4face6e83f2ca864932ec
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2023-07-04 22:44:46 +0300

    Damager update commit

--- Assets/Script/Damager.cs
+++ Assets/Script/Damager.cs
@@ -5,16 +5,7 @@ namespace Script
     public class Damager : MonoBehaviour
     {
         [SerializeField] int damage;
-        void Start()
-        {
-        
-        }
-
-        void Update()
-        {
-        
-        }
-        void OnCollisionEnter2D(Collision2D other)
+        void OnCollisionStay2D(Collision2D other)
         {
             HitPoints character = other.gameObject.GetComponent<HitPoints>();
             if(character != null){
