6bac5bf1612b849d85abf75f5f5c0e1e20b336cc
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2023-08-01 11:56:08 +0300

    Add function NewGame()

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,6 +6,11 @@ namespace UI
 {
     public class UIMainMenu : MonoBehaviour
     {
+        public void NewGame()
+        {
+            SerializationBinaryFormatter.DeleteData();
+            PlayGame();
+        }
         public void PlayGame()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
