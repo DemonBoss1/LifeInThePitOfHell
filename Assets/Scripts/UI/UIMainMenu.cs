574d7034f4ad341284bb0bd9458efbe217222033
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2023-08-01 13:00:58 +0300

    Add Warning New Game

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -1,11 +1,24 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 namespace UI
 {
     public class UIMainMenu : MonoBehaviour
     {
+        [SerializeField] private GameObject warningNewGame;
+
+        public void WarningNewGameOn()
+        {
+            warningNewGame.SetActive(true);
+        }
+
+        public void WarningNewGameOff()
+        {
+            warningNewGame.SetActive(false);
+
+        }
         public void NewGame()
         {
             SerializationBinaryFormatter.DeleteData();
