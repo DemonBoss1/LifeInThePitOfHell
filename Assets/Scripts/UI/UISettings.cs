6ed235143c0192c9d28ac27949234fa31aa113e2
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2023-08-02 12:00:08 +0300

    Save prev volume

--- Assets/Scripts/UI/UISettings.cs
+++ Assets/Scripts/UI/UISettings.cs
@@ -1,15 +1,32 @@
+using System;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class UISettings : MonoBehaviour
     {
+        private float _volume;
+        
         [SerializeField] private AudioMixer audioMixer;
+        [SerializeField] private Slider slider;
 
         public void SetVolume(float volume)
         {
+            _volume = volume;
             audioMixer.SetFloat("volume", volume);
+        }
+
+        private void Start()
+        {
+            _volume = PlayerPrefs.GetFloat("volume", 0);
+            slider.value = _volume;
+        }
+
+        private void OnDisable()
+        {
+            PlayerPrefs.SetFloat("volume", _volume);
         }
     }
 }
